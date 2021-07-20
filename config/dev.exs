818e371679a9f059ba8a590647d85e8c8b3b7bcb
Marc Lacoursiere <marc@roosoft.com>
2021-07-20 14:31:39 -0400

    changed dev postgresql password

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :steer, Steer.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "Pass2020!",
   database: "steer_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
