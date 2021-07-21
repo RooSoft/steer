487cc763a4993799b5976486ccc535de619a7131
Marc Lacoursiere <marc@roosoft.com>
2021-07-21 19:32:11 -0400

    easier to read formatting

--- lib/steer_web/live/page_live.ex
+++ lib/steer_web/live/page_live.ex
@@ -4,39 +4,18 @@ defmodule SteerWeb.PageLive do
   @impl true
   def mount(_params, _session, socket) do
     socket = socket
-    |> assign(:channels, LndClient.get_channels().channels)
+    |> assign(:channels, get_channels())
 
     {:ok, socket}
   end
 
-  @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
-  end
+  defp get_channels() do
+    LndClient.get_channels().channels
+    |> Enum.map(fn channel ->
+      node_info = LndClient.get_node_info(channel.remote_pubkey)
 
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
-
-  defp search(query) do
-    if not SteerWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+      channel
+      |> Map.put(:alias, node_info.node.alias)
+    end)
   end
 end
