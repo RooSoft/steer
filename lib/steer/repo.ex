378dc7ed5bf9018d872079df9c73b28d28e1d670
Marc Lacoursiere <marc@roosoft.com>
2021-08-06 12:42:00 -0400

    allow DATABASE_URL environment variable

--- lib/steer/repo.ex
+++ lib/steer/repo.ex
@@ -1,5 +1,18 @@
 defmodule Steer.Repo do
+  require Logger
   use Ecto.Repo,
     otp_app: :steer,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_type, config) do
+    database_url = System.get_env("DATABASE_URL")
+    if database_url == nil do
+      Logger.debug "$DATABASE_URL not set, using config"
+      {:ok, config}
+    else
+      Logger.debug "Configuring database using $DATABASE_URL"
+      Logger.debug database_url
+      {:ok, Keyword.put(config, :url, database_url)}
+    end
+  end
 end
