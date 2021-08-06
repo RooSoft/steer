734c9a694daf993c8c075acd22a91fb8c159037f
Marc Lacoursiere <marc@roosoft.com>
2021-08-06 12:42:44 -0400

    change default postgresql password to postgres

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :steer, Steer.Repo,
   username: "postgres",
-  password: "Pass2020!",
+  password: "postgres",
   database: "steer_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
