7510d51f658cf050973cbf1783d87e0f4891bf13
Marc Lacoursiere <marc@roosoft.com>
2021-08-09 16:22:57 -0400

    removed useless mount

--- lib/steer_web/live/channel_live/show.ex
+++ lib/steer_web/live/channel_live/show.ex
@@ -2,11 +2,6 @@ defmodule SteerWeb.ChannelLive.Show do
   use SteerWeb, :live_view
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
-  end
-
-  @impl true
   def handle_params(%{"id" => id_string}, _, socket) do
     { id, _ } = Integer.parse(id_string)
 
