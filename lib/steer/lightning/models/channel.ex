61e049b365ffa50b447fdb58941ba4ccab03a420
RooSoft <marc@roosoft.com>
2021-08-12 09:15:27 -0400

    added lightning channel model

---
+++ lib/steer/lightning/models/channel.ex
@@ -0,0 +1,22 @@
+defmodule Steer.Lightning.Models.Channel do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "channel" do
+    field :lnd_id, :decimal
+    field :channel_point, :string
+    field :node_pub_key, :string
+    field :is_active, :boolean
+    field :alias, :string
+    field :color, :string
+    field :capacity, :decimal
+    field :local_balance, :decimal
+    field :remote_balance, :decimal
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:lnd_id, :channel_point, :node_pub_key, :is_active, :alias, :color, :capacity, :local_balance, :remote_balance])
+    |> validate_required([:lnd_id, :channel_point, :node_pub_key, :is_active, :capacity, :local_balance, :remote_balance])
+  end
+end
