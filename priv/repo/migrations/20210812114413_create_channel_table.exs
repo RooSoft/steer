5f632f7cdad10d08132b2ec849c956b2386d320d
Marc Lacoursiere <marc@roosoft.com>
2021-08-12 08:03:25 -0400

    added creat_channel_table migration

---
+++ priv/repo/migrations/20210812114413_create_channel_table.exs
@@ -0,0 +1,20 @@
+defmodule Steer.Repo.Migrations.CreateChannelTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:channel) do
+      add :lnd_id, :decimal, null: false
+      add :channel_point, :string, null: false
+      add :node_pub_key, :string, null: false
+      add :is_active, :boolean, null: false
+      add :alias, :string
+      add :color, :string
+      add :capacity, :decimal, null: false
+      add :local_balance, :decimal, null: false
+      add :remote_balance, :decimal, null: false
+    end
+
+    create unique_index(:channel, [:lnd_id])
+    create unique_index(:channel, [:channel_point])
+  end
+end
