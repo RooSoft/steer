4e6557f9e317142c68cdba0b869c1766d68fec65
RooSoft <marc@roosoft.com>
2021-08-12 09:04:05 -0400

    #3 add create_forward_table migration

--- mix.exs
+++ mix.exs
@@ -61,6 +61,7 @@ defmodule Steer.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:number, "~>  1.0.3"},
       {:timex, "~> 3.0"},
+      {:ecto_enum, "~> 1.4"},
       {:lnd_client, git: "https://github.com/RooSoft/lnd_client.git", tag: "0.0.3"}
     ]
   end
