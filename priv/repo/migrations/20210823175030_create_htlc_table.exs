d5d24f28776333b364ca847e6b6393633908308c
RooSoft <marc@roosoft.com>
2021-08-23 14:15:33 -0400

    the htlc table isnt required anymore

--- priv/repo/migrations/20210823175030_create_htlc_table.exs
+++
@@ -1,9 +0,0 @@
-defmodule Steer.Repo.Migrations.CreateHtlcTable do
-  use Ecto.Migration
-
-  def change do
-    create table(:htlc) do
-      timestamps()
-    end
-  end
-end
--- priv/repo/migrations/20210823175332_create_htlc_event_table.exs
+++ priv/repo/migrations/20210823175332_create_htlc_event_table.exs
@@ -10,8 +10,8 @@ defmodule Steer.Repo.Migrations.CreateHtlcEventTable do
       add :type, :htlc_event_type, values: [:forward, :forward_fail, :settle, :link_fail], null: false
       add :channel_in_id, references(:channel), null: false
       add :channel_out_id, references(:channel), null: false
-      add :htlc_in_id, references(:htlc), null: false
-      add :htlc_out_id, references(:htlc), null: false
+      add :htlc_in_id, :bigint, null: false
+      add :htlc_out_id, :bigint, null: false
       add :time, :naive_datetime_usec, null: false
       add :timestamp_ns, :bigint, null: false
 
