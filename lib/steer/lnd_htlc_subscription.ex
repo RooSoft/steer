e2bcdf9d0c3a7485715adb86d75ef8f16040e42c
RooSoft <marc@roosoft.com>
2021-08-24 13:41:52 -0400

    #4 create htlc_link_fail table

--- lib/steer/lnd_htlc_subscription.ex
+++ lib/steer/lnd_htlc_subscription.ex
@@ -30,6 +30,8 @@ defmodule Steer.HtlcSubscription do
   def handle_info(%Routerrpc.HtlcEvent{ event: {:forward_event, forward_event } } = lnd_htlc_event, state) do
     Logger.info "NEW HTLC: forward event"
 
+    IO.inspect lnd_htlc_event
+
     htlc_event = lnd_htlc_event
     |> extract_htlc_event_map(:forward)
     |> Steer.Lightning.insert_htlc_event
@@ -42,10 +44,22 @@ defmodule Steer.HtlcSubscription do
     {:noreply, state}
   end
 
-  def handle_info(%Routerrpc.HtlcEvent{ event: {:forward_fail_event, _ } } = htlc_event, state) do
+  def handle_info(%Routerrpc.HtlcEvent{ event: {:forward_fail_event, _ } } = lnd_htlc_event, state) do
     Logger.info "NEW HTLC: forward fail event"
 
-    IO.inspect htlc_event
+    IO.inspect lnd_htlc_event
+
+    {:noreply, state}
+  end
+
+  def handle_info(%Routerrpc.HtlcEvent{ event: {:link_fail_event, _ } } = lnd_htlc_event, state) do
+    Logger.info "NEW HTLC: link fail event"
+
+    IO.inspect lnd_htlc_event
+
+    lnd_htlc_event
+    |> extract_htlc_event_map(:link_fail)
+    |> Steer.Lightning.insert_htlc_event
 
     {:noreply, state}
   end
@@ -93,5 +107,9 @@ defmodule Steer.HtlcSubscription do
       timelock_in: incoming_timelock,
       timelock_out: outgoing_timelock
     }
+  end
+
+  defp extract_link_fail_event_map %{ } do
+    %{}
   end
 end
--- priv/repo/migrations/20210824134939_create_htlc_forward_table.exs
+++ priv/repo/migrations/20210824134939_create_htlc_forward_table.exs
@@ -6,7 +6,7 @@ defmodule Steer.Repo.Migrations.CreateHtlcForwardTable do
       add :amount_in, :bigint, null: false
       add :amount_out, :bigint, null: false
       add :timelock_in, :bigint, null: false
-      add :timelock_out, :bigint, nuill: false
+      add :timelock_out, :bigint, null: false
       add :htlc_event_id, references(:htlc_event), null: false
 
       timestamps()
