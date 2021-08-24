9fdb7322517739730c3afdd2072a29248d68ce69
RooSoft <marc@roosoft.com>
2021-08-24 13:42:00 -0400

    #4 create htlc_link_fail table

---
+++ priv/repo/migrations/20210824173906_create_htlc_link_fail_table.exs
@@ -0,0 +1,20 @@
+defmodule Steer.Repo.Migrations.CreateHtlcLinkFailTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:htlc_link_fail) do
+      add :amount_in, :bigint, null: false
+      add :amount_out, :bigint, null: false
+      add :timelock_in, :bigint, null: false
+      add :timelock_out, :bigint, null: false
+
+      add :wire_failure, :string, null: false
+      add :failure_detail, :string, null: false
+      add :failure_string, :string, null: false
+
+      add :htlc_event_id, references(:htlc_event), null: false
+
+      timestamps()
+    end
+  end
+end
