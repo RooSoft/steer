a19256196bace4ce92143f241f4255a290ba2f30
RooSoft <marc@roosoft.com>
2021-08-24 10:10:39 -0400

    added HtlcForward model

---
+++ lib/steer/lightning/models/htlc_forward.ex
@@ -0,0 +1,21 @@
+defmodule Steer.Lightning.Models.HtlcForward do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Steer.Lightning.Models, as: Models
+
+  schema "htlc_event" do
+    belongs_to :htlc_event, Models.HtlcEvent
+    field :amount_in, :integer
+    field :amount_out, :integer
+    field :timelock_in, :integer
+    field :timelock_out, :integer
+
+    timestamps()
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:htlc_event_id, :amount_in, :amount_out, :timelock_in, :timelock_out])
+    |> validate_required([:htlc_event_id, :amount_in, :amount_out, :timelock_in, :timelock_out])
+  end
+end
