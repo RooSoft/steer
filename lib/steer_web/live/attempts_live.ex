eaa275e6e9f875484259aac0f5659938dc77bb68
RooSoft <marc@roosoft.com>
2021-08-27 10:41:58 -0400

    display attemtpts settlement status

--- lib/steer_web/live/attempts_live.ex
+++ lib/steer_web/live/attempts_live.ex
@@ -9,6 +9,7 @@ defmodule SteerWeb.AttemptsLive do
       socket
       |> assign_attempts
       |> format_channels
+      |> format_statuses
     }
   end
 
@@ -28,10 +29,33 @@ defmodule SteerWeb.AttemptsLive do
   defp format_local_node %{ channel_in_id: nil } = attempt do
     attempt
     |> Map.put(:channel_in, "Me")
-    |> IO.inspect
   end
 
   defp format_local_node attempt do
     attempt
+  end
+
+  defp format_statuses(%{ assigns: %{ attempts: attempts } } = socket) do
+    new_attempts = attempts
+    |> Enum.map(&format_status/1)
+
+    socket
+    |> assign(:attempts, new_attempts)
+  end
+
+  defp format_status(%{ fail_id: fail_id } = attempt) when is_number(fail_id) do
+    attempt
+    |> Map.put(:status, "failed")
+  end
+
+  defp format_status(%{ settle_id: settle_id } = attempt) when is_number(settle_id) do
+    attempt
+    |> Map.put(:status, "settled")
+  end
+
+  defp format_status attempt do
+    attempt
+    |> Map.put(:status, "unknown")
+    |> IO.inspect
   end
 end
