4e89b9196ed67805af3f42acad0fb126f7cd3340
RooSoft <marc@roosoft.com>
2021-08-27 10:06:12 -0400

    created the get_htlc_forwards_with_statuses in Repo

--- lib/steer/repo.ex
+++ lib/steer/repo.ex
@@ -95,6 +95,37 @@ defmodule Steer.Repo do
       preload: [:channel_in, :channel_out]
   end
 
+  def get_htlc_forwards_with_statuses do
+    all from hf in Models.HtlcForward,
+      join: htlc in Models.HtlcEvent, on:
+        hf.htlc_event_id == htlc.id,
+      left_join: fail in Models.HtlcEvent, on:
+        fail.htlc_in_id == htlc.htlc_in_id
+        and fail.htlc_out_id == htlc.htlc_out_id
+        and fail.type == :forward_fail,
+      left_join: settle in Models.HtlcEvent, on:
+        fail.htlc_in_id == htlc.htlc_in_id
+        and fail.htlc_out_id == htlc.htlc_out_id
+        and fail.type == :settle,
+      left_join: ci in Models.Channel, on:
+        ci.id == htlc.channel_in_id,
+      left_join: co in Models.Channel, on:
+        co.id == htlc.channel_out_id,
+      order_by: [desc: htlc.time],
+      select: %{
+        htlc_id: htlc.id,
+        fail_id: fail.id,
+        settle_id: settle.id,
+        channel_in_id: ci.id,
+        channel_in: ci.alias,
+        channel_out_id: co.id,
+        channel_out: co.alias,
+        amount_in: hf.amount_in,
+        amount_out: hf.amount_out,
+        time: htlc.time
+      }
+  end
+
   def mark_forwards_as_consolidated forward_ids do
     query =
       from f in Models.Forward,
