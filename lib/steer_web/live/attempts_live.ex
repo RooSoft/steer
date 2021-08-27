5e8aca6f411f0690c5a472b5b541249f7c45eb69
RooSoft <marc@roosoft.com>
2021-08-27 10:33:59 -0400

    display Me instead of nothing for the current node in attempts

--- lib/steer_web/live/attempts_live.ex
+++ lib/steer_web/live/attempts_live.ex
@@ -7,11 +7,31 @@ defmodule SteerWeb.AttemptsLive do
   def mount(_params, _session, socket) do
     {:ok,
       socket
-      |> assign_attempts}
+      |> assign_attempts
+      |> format_channels
+    }
   end
 
   defp assign_attempts(socket) do
     socket
     |> assign(:attempts, Steer.Lightning.get_htlc_forwards_with_statuses())
+  end
+
+  defp format_channels(%{ assigns: %{ attempts: attempts } } = socket) do
+    new_attempts = attempts
+    |> Enum.map(&format_local_node/1)
+
+    socket
+    |> assign(:attempts, new_attempts)
+  end
+
+  defp format_local_node %{ channel_in_id: nil } = attempt do
+    attempt
+    |> Map.put(:channel_in, "Me")
+    |> IO.inspect
+  end
+
+  defp format_local_node attempt do
+    attempt
   end
 end
