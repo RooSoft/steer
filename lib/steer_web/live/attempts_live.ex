a71f7a9d26c53caa8f9d81c7b0d29fbaba6b105b
RooSoft <marc@roosoft.com>
2021-08-27 10:24:26 -0400

    displaying attempts in a basic format

--- lib/steer_web/live/attempts_live.ex
+++ lib/steer_web/live/attempts_live.ex
@@ -5,6 +5,13 @@ defmodule SteerWeb.AttemptsLive do
   @impl true
   @spec mount(any, any, Phoenix.LiveView.Socket.t()) :: {:ok, Phoenix.LiveView.Socket.t()}
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+      socket
+      |> assign_attempts}
+  end
+
+  defp assign_attempts(socket) do
+    socket
+    |> assign(:attempts, Steer.Lightning.get_htlc_forwards_with_statuses())
   end
 end
