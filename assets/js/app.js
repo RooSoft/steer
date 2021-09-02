2fc9280b709cd8e47d78eeae39cf0ec61a0ff265
RooSoft <marc@roosoft.com>
2021-09-02 15:32:21 -0400

    properly update alpine when assign changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,10 @@ let liveSocket = new LiveSocket("/live", Socket, {
     params: {_csrf_token: csrfToken},
     dom: {
         onBeforeElUpdated(from, to){
-            if(from.__x){ window.Alpine.clone(from.__x, to) }
+            if(from._x_dataStack){ 
+                window.Alpine.clone(from, to)
+                window.Alpine.initTree(to)
+            }
         }
     }
 })
