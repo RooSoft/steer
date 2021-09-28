d12333c0f29f81030a4115d07ec0e76c23c95fff
RooSoft <marc@roosoft.com>
2021-09-28 16:09:35 -0400

    added some formatting on diagnostic page, button still enabled while connecting

--- lib/steer_web/live/diagnostics_live.ex
+++ lib/steer_web/live/diagnostics_live.ex
@@ -5,25 +5,37 @@ defmodule SteerWeb.DiagnosticsLive do
   @impl true
   @spec mount(any, any, Phoenix.LiveView.Socket.t()) :: {:ok, Phoenix.LiveView.Socket.t()}
   def mount(_params, _session, socket) do
-    { :ok, socket }
+    { :ok, socket
+      |> assign(:messages, [%{text: "this is a message"}]) }
   end
 
 
   @impl true
   def handle_event("connect", _value, socket) do
-    Logger.info("Trying to connect to the node...")
-
-    # If the connect method fails, Steer.Lightning will crash and be restarted by the supervisor
-    case Steer.Lightning.connect() do
+    socket = case Steer.Lightning.connect() do
       :ok ->
-        Logger.info("Node connection successful, doing sync and updating cache")
+        socket = socket |> dispatch_message("Trying to connect to the node...")
+
         Steer.Lightning.sync()
         Steer.Lightning.update_cache()
+
+        socket |> dispatch_message("Node connection successful")
       _ ->
-        Logger.info("Node connection failed")
+        socket |> dispatch_message("Node connection failed")
     end
 
-    { :noreply, socket  }
+    { :noreply, socket }
   end
 
+  defp dispatch_message(socket, message) do
+    Logger.info(message)
+    socket |> add_message(message)
+  end
+
+  defp add_message(socket, message_text) do
+    message = %{text: message_text}
+
+    socket
+    |> assign(:messages, [message|socket.assigns.messages])
+  end
 end
