e94fc86f8d76c84d9a200717c449afb355d7ef51
RooSoft <marc@roosoft.com>
2022-01-18 09:39:52 -0500

    added indicators component

--- lib/steer_web/live/channel_live/list_component.ex
+++ lib/steer_web/live/channel_live/list_component.ex
@@ -1,8 +1,11 @@
 defmodule SteerWeb.ChannelLive.ListComponent do
   use SteerWeb, :live_component
 
+  import SteerWeb.Components.IndicatorsComponent
+
   def mount(_params, _session, socket) do
-    { :ok, socket
-      |> assign(:id, 0) }
+    {:ok,
+     socket
+     |> assign(:id, 0)}
   end
 end
