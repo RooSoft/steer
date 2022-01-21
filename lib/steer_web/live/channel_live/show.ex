10859e90db5a16d98746507a13734667b613588d
RooSoft <marc@roosoft.com>
2022-01-21 09:35:57 -0500

    styled show.html.heex

--- lib/steer_web/live/channel_live/show.ex
+++ lib/steer_web/live/channel_live/show.ex
@@ -12,71 +12,80 @@ defmodule SteerWeb.ChannelLive.Show do
   @active_message "active"
   @inactive_message "inactive"
 
+  import SteerWeb.Components.NodeStatusIndicatorComponent
+
   @impl true
   def handle_params(%{"id" => channel_id_string}, _, socket) do
-    { channel_id, _ } = Integer.parse(channel_id_string)
+    {channel_id, _} = Integer.parse(channel_id_string)
 
     {:noreply,
-      socket
-      |> update_socket(channel_id)
-      |> subscribe_to_events()}
+     socket
+     |> update_socket(channel_id)
+     |> subscribe_to_events()}
   end
 
   @impl true
-  def handle_info(%{
-    topic: @htlc_event_topic,
-    event: @settle_message,
-    payload: _htlc_event
-  }, socket) do
-
-    { :noreply, socket
-      |> update_socket
-      |> put_flash(:info, "Some HTLC settle event happened")}
+  def handle_info(
+        %{
+          topic: @htlc_event_topic,
+          event: @settle_message,
+          payload: _htlc_event
+        },
+        socket
+      ) do
+    {:noreply,
+     socket
+     |> update_socket
+     |> put_flash(:info, "Some HTLC settle event happened")}
   end
 
-  def handle_info(%{
-    topic: @htlc_event_topic,
-    event: _message,
-    payload: _htlc_event
-  }, socket) do
-
+  def handle_info(
+        %{
+          topic: @htlc_event_topic,
+          event: _message,
+          payload: _htlc_event
+        },
+        socket
+      ) do
     # ignore these message types
 
-    { :noreply, socket }
+    {:noreply, socket}
   end
 
   @impl true
-  def handle_info(%{ topic: @channel_topic, event: @active_message, payload: channel }, socket) do
-    { :noreply, socket
-      |> update_socket
-      |> put_flash(:info, "#{channel.alias} became active")}
+  def handle_info(%{topic: @channel_topic, event: @active_message, payload: channel}, socket) do
+    {:noreply,
+     socket
+     |> update_socket
+     |> put_flash(:info, "#{channel.alias} became active")}
   end
 
   @impl true
-  def handle_info(%{ topic: @channel_topic, event: @inactive_message, payload: channel }, socket) do
-    socket = socket
+  def handle_info(%{topic: @channel_topic, event: @inactive_message, payload: channel}, socket) do
+    socket =
+      socket
       |> update_socket
       |> put_flash(:info, "#{channel.alias} became inactive")
 
-    { :noreply, socket }
+    {:noreply, socket}
   end
 
   @impl true
-  def handle_info(%{ topic: @channel_topic, event: _, payload: _channel }, socket) do
+  def handle_info(%{topic: @channel_topic, event: _, payload: _channel}, socket) do
     # ingnore all other events
 
-    { :noreply, socket }
+    {:noreply, socket}
   end
 
-  defp update_socket socket do
+  defp update_socket(socket) do
     socket
     |> update_socket(socket.assigns.channel.id)
   end
 
-  defp update_socket socket, channel_id do
+  defp update_socket(socket, channel_id) do
     socket
     |> assign(:channel, Steer.Lightning.get_channel(id: channel_id))
-    |> assign(:forwards, Steer.Lightning.get_channel_forwards(%{ channel_id: channel_id }))
+    |> assign(:forwards, Steer.Lightning.get_channel_forwards(%{channel_id: channel_id}))
   end
 
   defp subscribe_to_events(socket) do
