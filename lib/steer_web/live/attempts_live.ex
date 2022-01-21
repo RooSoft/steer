b1120cd004979a37e9643e582a6c46308649d812
RooSoft <marc@roosoft.com>
2022-01-21 11:35:02 -0500

    forward attempts are now legible

--- lib/steer_web/live/attempts_live.ex
+++ lib/steer_web/live/attempts_live.ex
@@ -12,37 +12,38 @@ defmodule SteerWeb.AttemptsLive do
   @spec mount(any, any, Phoenix.LiveView.Socket.t()) :: {:ok, Phoenix.LiveView.Socket.t()}
   def mount(_params, _session, socket) do
     {:ok,
-      socket
-      |> init_attempts
-      |> load
-      |> subscribe_to_events
-    }
+     socket
+     |> init_attempts
+     |> load
+     |> subscribe_to_events}
   end
 
   @impl true
   def handle_event("load-more", %{"htlc-id" => htlc_id}, socket) do
-    Logger.info "load more... from #{htlc_id}"
+    Logger.info("load more... from #{htlc_id}")
 
     {:noreply,
-      socket
-      |> load(from_forward_htlc_id: htlc_id)}
+     socket
+     |> load(from_forward_htlc_id: htlc_id)}
   end
 
   @impl true
-  def handle_info(%{
-    topic: @htlc_event_topic,
-    event: event,
-    payload: htlc_event
-  }, socket) do
-
-    { :noreply,
-      socket
-      |> prepend_attempt(htlc_event.id)
-      |> format_channels
-      |> format_statuses
-      |> format_amounts
-      |> flag_last
-      |> put_flash(:info, "Some HTLC settle event happened: #{event}")}
+  def handle_info(
+        %{
+          topic: @htlc_event_topic,
+          event: event,
+          payload: htlc_event
+        },
+        socket
+      ) do
+    {:noreply,
+     socket
+     |> prepend_attempt(htlc_event.id)
+     |> format_channels
+     |> format_statuses
+     |> format_amounts
+     |> flag_last
+     |> put_flash(:info, "Some HTLC settle event happened: #{event}")}
   end
 
   defp init_attempts(socket) do
@@ -51,9 +52,11 @@ defmodule SteerWeb.AttemptsLive do
   end
 
   defp prepend_attempt(socket, htlc_id) do
-    [attempt] = Steer.Lightning.get_htlc_forwards_with_statuses(
-      from_forward_htlc_id: htlc_id, limit: 1
-    )
+    [attempt] =
+      Steer.Lightning.get_htlc_forwards_with_statuses(
+        from_forward_htlc_id: htlc_id,
+        limit: 1
+      )
 
     socket
     |> assign(:attempts, [attempt | socket.assigns.attempts])
@@ -87,70 +90,78 @@ defmodule SteerWeb.AttemptsLive do
     |> assign(:attempts, current_attempts ++ new_attempts)
   end
 
-  defp flag_last socket do
+  defp flag_last(socket) do
     attempts = socket.assigns.attempts
 
-    number_of_attempts = attempts |> Enum.count
+    number_of_attempts = attempts |> Enum.count()
 
     socket
-    |> assign(:attempts,
-      Enum.with_index(attempts, 1) |> Enum.map(fn { attempt, index } ->
+    |> assign(
+      :attempts,
+      Enum.with_index(attempts, 1)
+      |> Enum.map(fn {attempt, index} ->
         attempt |> Map.put(:is_last, index == number_of_attempts)
       end)
     )
   end
 
-  defp format_channels(%{ assigns: %{ attempts: attempts } } = socket) do
-    new_attempts = attempts
-    |> Enum.map(&format_local_node/1)
+  defp format_channels(%{assigns: %{attempts: attempts}} = socket) do
+    new_attempts =
+      attempts
+      |> Enum.map(&format_local_node/1)
 
     socket
     |> assign(:attempts, new_attempts)
   end
 
-  defp format_local_node %{ channel_in_id: nil } = attempt do
+  defp format_local_node(%{channel_in_id: nil} = attempt) do
     attempt
     |> Map.put(:channel_in, "Me")
   end
 
-  defp format_local_node attempt do
+  defp format_local_node(attempt) do
     attempt
   end
 
-  defp format_statuses(%{ assigns: %{ attempts: attempts } } = socket) do
-    new_attempts = attempts
-    |> Enum.map(&format_status/1)
+  defp format_statuses(%{assigns: %{attempts: attempts}} = socket) do
+    new_attempts =
+      attempts
+      |> Enum.map(&format_status/1)
 
     socket
     |> assign(:attempts, new_attempts)
   end
 
-  defp format_status(%{ fail_id: fail_id } = attempt) when is_number(fail_id) do
+  defp format_status(%{fail_id: fail_id} = attempt) when is_number(fail_id) do
     attempt
     |> Map.put(:status, "failed")
+    |> Map.put(:status_class, "status-failed")
   end
 
-  defp format_status(%{ settle_id: settle_id } = attempt) when is_number(settle_id) do
+  defp format_status(%{settle_id: settle_id} = attempt) when is_number(settle_id) do
     attempt
     |> Map.put(:status, "settled")
+    |> Map.put(:status_class, "status-settled")
   end
 
-  defp format_status attempt do
+  defp format_status(attempt) do
     attempt
     |> Map.put(:status, "unknown")
+    |> Map.put(:status_class, "status-unknown")
   end
 
-  defp format_amounts(%{ assigns: %{ attempts: attempts } } = socket) do
-    new_attempts = attempts
-    |> Enum.map(&format_amount/1)
+  defp format_amounts(%{assigns: %{attempts: attempts}} = socket) do
+    new_attempts =
+      attempts
+      |> Enum.map(&format_amount/1)
 
     socket
     |> assign(:attempts, new_attempts)
   end
 
   defp format_amount(attempt) do
-    amount_in_sats = attempt.amount_in/1000
-    amount_out_sats = attempt.amount_out/1000
+    amount_in_sats = attempt.amount_in / 1000
+    amount_out_sats = attempt.amount_out / 1000
 
     attempt
     |> Map.put(
@@ -160,6 +171,6 @@ defmodule SteerWeb.AttemptsLive do
     |> Map.put(
       :formatted_amount_out,
       Number.SI.number_to_si(amount_out_sats, unit: "", precision: 0)
-      )
+    )
   end
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Steer.MixProject do
   def project do
     [
       app: :steer,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
