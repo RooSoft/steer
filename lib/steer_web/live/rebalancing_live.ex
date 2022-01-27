46ed40474617d08bb297bef46cf7945f4cbd5303
RooSoft <marc@roosoft.com>
2022-01-27 18:35:57 -0500

    display debugging information in the rebalancing console

--- lib/steer_web/live/rebalancing_live.ex
+++ lib/steer_web/live/rebalancing_live.ex
@@ -2,6 +2,8 @@ defmodule SteerWeb.RebalancingLive do
   use SteerWeb, :live_view
   require Logger
 
+  alias Steer.Formatting.Sats
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok,
@@ -12,9 +14,27 @@ defmodule SteerWeb.RebalancingLive do
   end
 
   @impl true
-  def handle_event("rebalancing-generate-igniter", data, socket) do
+  def handle_event(
+        "rebalancing-generate-igniter",
+        %{"selectedLowId" => selectedLowId, "selectedHighId" => selectedHighId},
+        socket
+      ) do
+    low_liquidity_node = Steer.Lightning.get_channel(id: selectedLowId)
+    high_liquidity_node = Steer.Lightning.get_channel(id: selectedHighId)
+
     IO.puts("---------- will send igniter script ---------")
-    IO.inspect(data)
+    IO.inspect(high_liquidity_node)
+
+    IO.puts(
+      "low: #{low_liquidity_node.alias} #{low_liquidity_node.node_pub_key} #{Sats.to_human(low_liquidity_node.local_balance)} #{low_liquidity_node.formatted_balance_percent}%"
+    )
+
+    IO.puts(
+      "high: #{high_liquidity_node.alias} #{high_liquidity_node.node_pub_key} #{Sats.to_human(high_liquidity_node.local_balance)} #{high_liquidity_node.formatted_balance_percent}%"
+    )
+
+    print_routes(10, low_liquidity_node, high_liquidity_node)
+
     IO.puts("---------------------------------------------")
 
     {:noreply, socket}
@@ -43,5 +63,35 @@ defmodule SteerWeb.RebalancingLive do
 
     socket
     |> assign(:high_liquidity_channels, high_liquidity_channels)
+  end
+
+  defp print_routes(route_count, low_liquidity_node, high_liquidity_node) do
+    Steer.GraphRepo.get_cheapest_routes(
+      route_count,
+      high_liquidity_node.node_pub_key,
+      low_liquidity_node.node_pub_key
+    )
+    |> Enum.each(fn route -> print_route(high_liquidity_node.lnd_id, route) end)
+
+    high_liquidity_node.lnd_id
+  end
+
+  defp print_route(outgoing_channel, route) do
+    IO.puts("AMOUNT=#{10000}")
+    IO.puts("MAX_FEE=#{100}")
+    IO.puts("OUTGOING_CHAN_ID=#{outgoing_channel}")
+
+    IO.puts("declare pub_keys=(")
+
+    route.pub_keys
+    |> Enum.each(&{IO.puts(&1)})
+
+    IO.puts("037b6d303c95b4faf2f62a214cc32c78aa0ded8ab5bd7a11aaa4883bbe292a4764")
+
+    IO.puts(")")
+
+    IO.puts("")
+    IO.puts("----------")
+    IO.puts("")
   end
 end
