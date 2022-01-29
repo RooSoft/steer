e7430aaff05a9820133b9506f413144da1b7a28e
RooSoft <marc@roosoft.com>
2022-01-28 20:13:44 -0500

    newly formatted summary displaying monetary info

--- lib/steer_web/live/rebalancing_live/summary.ex
+++ lib/steer_web/live/rebalancing_live/summary.ex
@@ -1,17 +1,28 @@
 defmodule SteerWeb.RebalancingLive.Summary do
   use Phoenix.Component
 
+  alias Steer.Formatting.Sats
+
   def summary(assigns) do
     ~H"""
     <div class="rebalancing-summary-header">
       What's going to happen...
     </div>
 
-    <div>
+    <div class="rebalancing-summary-paragraph">
       Trying to rebalance from
       <em><%= maybe_show_alias assigns.high_liquidity_channel%></em>
       to
       <em><%= maybe_show_alias assigns.low_liquidity_channel%></em>
+    </div>
+
+    <div class="rebalancing-summary-paragraph">
+      <div>
+        from: <%= channel_stats assigns.high_liquidity_channel %>
+      </div>
+      <div>
+        to: <%= channel_stats assigns.low_liquidity_channel %>
+      </div>
     </div>
     """
   end
@@ -20,7 +31,20 @@ defmodule SteerWeb.RebalancingLive.Summary do
     ""
   end
 
-  def maybe_show_alias(%{alias: channnel_alias}) do
-    channnel_alias
+  def maybe_show_alias(%{alias: node_alias}) do
+    node_alias
+  end
+
+  def channel_stats(nil) do
+    ""
+  end
+
+  def channel_stats(%{
+        alias: node_alias,
+        node_pub_key: _node_pub_key,
+        local_balance: local_balance,
+        formatted_balance_percent: formatted_balance_percent
+      }) do
+    "#{node_alias} #{Sats.to_human(local_balance)} #{formatted_balance_percent}%"
   end
 end
