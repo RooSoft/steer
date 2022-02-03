1deb092ff11b2a21c321d241ef2434703cd81dfc
RooSoft <marc@roosoft.com>
2022-02-03 14:53:39 -0500

    removed old comments

--- lib/steer/lnd/subscriptions/htlc.ex
+++ lib/steer/lnd/subscriptions/htlc.ex
@@ -11,7 +11,7 @@ defmodule Steer.Lnd.Subscriptions.Htlc do
   @link_fail_message "link_fail"
 
   def start() do
-    { :ok, subscription } = GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+    {:ok, subscription} = GenServer.start_link(__MODULE__, nil, name: __MODULE__)
 
     Process.monitor(subscription)
   end
@@ -21,81 +21,89 @@ defmodule Steer.Lnd.Subscriptions.Htlc do
   end
 
   def init(_) do
-    IO.puts "++++++++++++++++++subscribing to HTLCs+++++++++++++++"
+    IO.puts("++++++++++++++++++subscribing to HTLCs+++++++++++++++")
     LndClient.subscribe_htlc_events(%{pid: self()})
 
-    { :ok, nil }
+    {:ok, nil}
   end
 
   def handle_info(%Routerrpc.HtlcEvent{event: {:settle_event, _}} = lnd_htlc_event, state) do
-    # Steer.Lightning.sync
-    # Steer.Lightning.update_cache
-    ### sync is being done on invoice settle instead
+    htlc_event =
+      lnd_htlc_event
+      |> extract_htlc_event_map(:settle)
+      |> Steer.Lightning.insert_htlc_event()
+      |> broadcast(@htlc_event_topic, @settle_message)
 
-    htlc_event = lnd_htlc_event
-    |> extract_htlc_event_map(:settle)
-    |> Steer.Lightning.insert_htlc_event
-    |> broadcast(@htlc_event_topic, @settle_message)
-
-    Logger.info "HTLC settle event \##{htlc_event.id}"
+    Logger.info("HTLC settle event \##{htlc_event.id}")
 
     {:noreply, state}
   end
 
-  def handle_info(%Routerrpc.HtlcEvent{ event: {:forward_event, lnd_forward_event } } = lnd_htlc_event, state) do
-    htlc_event = lnd_htlc_event
-    |> extract_htlc_event_map(:forward)
-    |> Steer.Lightning.insert_htlc_event
+  def handle_info(
+        %Routerrpc.HtlcEvent{event: {:forward_event, lnd_forward_event}} = lnd_htlc_event,
+        state
+      ) do
+    htlc_event =
+      lnd_htlc_event
+      |> extract_htlc_event_map(:forward)
+      |> Steer.Lightning.insert_htlc_event()
 
-    forward_event = lnd_forward_event
-    |> extract_forward_event_map()
-    |> Map.put(:htlc_event_id, htlc_event.id)
-    |> Steer.Lightning.insert_htlc_forward
-    |> broadcast(@htlc_event_topic, @forward_message)
+    forward_event =
+      lnd_forward_event
+      |> extract_forward_event_map()
+      |> Map.put(:htlc_event_id, htlc_event.id)
+      |> Steer.Lightning.insert_htlc_forward()
+      |> broadcast(@htlc_event_topic, @forward_message)
 
-    Logger.info "HTLC forward event \##{forward_event.id}"
-
-    {:noreply, state}
-  end
-
-  def handle_info(%Routerrpc.HtlcEvent{ event: {:forward_fail_event, _ } } = lnd_htlc_event, state) do
-    htlc_event = lnd_htlc_event
-    |> extract_htlc_event_map(:forward_fail)
-    |> Steer.Lightning.insert_htlc_event
-    |> broadcast(@htlc_event_topic, @forward_fail_message)
-
-    Logger.info "HTLC forward fail event \##{htlc_event.id}"
+    Logger.info("HTLC forward event \##{forward_event.id}")
 
     {:noreply, state}
   end
 
-  def handle_info(%Routerrpc.HtlcEvent{ event: {:link_fail_event, lnd_link_fail_event } } = lnd_htlc_event, state) do
-    htlc_event = lnd_htlc_event
-    |> extract_htlc_event_map(:link_fail)
-    |> Steer.Lightning.insert_htlc_event
+  def handle_info(%Routerrpc.HtlcEvent{event: {:forward_fail_event, _}} = lnd_htlc_event, state) do
+    htlc_event =
+      lnd_htlc_event
+      |> extract_htlc_event_map(:forward_fail)
+      |> Steer.Lightning.insert_htlc_event()
+      |> broadcast(@htlc_event_topic, @forward_fail_message)
 
-    link_fail_event = lnd_link_fail_event
-    |> extract_link_fail_event_map()
-    |> Map.put(:htlc_event_id, htlc_event.id)
-    |> Steer.Lightning.insert_htlc_link_fail()
-    |> broadcast(@htlc_event_topic, @link_fail_message)
+    Logger.info("HTLC forward fail event \##{htlc_event.id}")
 
-    Logger.info "HTLC link fail event \##{link_fail_event.id}"
+    {:noreply, state}
+  end
+
+  def handle_info(
+        %Routerrpc.HtlcEvent{event: {:link_fail_event, lnd_link_fail_event}} = lnd_htlc_event,
+        state
+      ) do
+    htlc_event =
+      lnd_htlc_event
+      |> extract_htlc_event_map(:link_fail)
+      |> Steer.Lightning.insert_htlc_event()
+
+    link_fail_event =
+      lnd_link_fail_event
+      |> extract_link_fail_event_map()
+      |> Map.put(:htlc_event_id, htlc_event.id)
+      |> Steer.Lightning.insert_htlc_link_fail()
+      |> broadcast(@htlc_event_topic, @link_fail_message)
+
+    Logger.info("HTLC link fail event \##{link_fail_event.id}")
 
     {:noreply, state}
   end
 
   def handle_info(%Routerrpc.HtlcEvent{} = htlc_event, state) do
-    Logger.info "HTLC of unknown type"
+    Logger.info("HTLC of unknown type")
 
-    IO.inspect htlc_event
+    IO.inspect(htlc_event)
 
     {:noreply, state}
   end
 
-  def handle_info({ :DOWN, _ref, :process, _subscription, reason}, state) do
+  def handle_info({:DOWN, _ref, :process, _subscription, reason}, state) do
     Logger.error("HTLC subscription is DOWN and shouldn't be")
-    IO.inspect reason
+    IO.inspect(reason)
     Logger.info("Restarting HTLC subscription")
 
     start()
@@ -104,13 +112,13 @@ defmodule Steer.Lnd.Subscriptions.Htlc do
   end
 
   def handle_info(event, state) do
-    Logger.info "--------- got an unknown event"
-    IO.inspect event
+    Logger.info("--------- got an unknown event")
+    IO.inspect(event)
 
     {:noreply, state}
   end
 
-  defp extract_htlc_event_map htlc_event, type do
+  defp extract_htlc_event_map(htlc_event, type) do
     in_channel_id = get_channel_id(htlc_event.incoming_channel_id)
     out_channel_id = get_channel_id(htlc_event.outgoing_channel_id)
     time = DateTime.from_unix!(htlc_event.timestamp_ns, :nanosecond)
@@ -126,23 +134,25 @@ defmodule Steer.Lnd.Subscriptions.Htlc do
     }
   end
 
-  defp get_channel_id 0 do
+  defp get_channel_id(0) do
     nil
   end
 
-  defp get_channel_id lnd_id do
+  defp get_channel_id(lnd_id) do
     case Steer.Lightning.get_channel(lnd_id: lnd_id) do
       nil -> nil
       channel -> channel.id
     end
   end
 
-  defp extract_forward_event_map %{ info: %Routerrpc.HtlcInfo{
-    incoming_amt_msat: incoming_amt_msat,
-    outgoing_amt_msat: outgoing_amt_msat,
-    incoming_timelock: incoming_timelock,
-    outgoing_timelock: outgoing_timelock
-  }} do
+  defp extract_forward_event_map(%{
+         info: %Routerrpc.HtlcInfo{
+           incoming_amt_msat: incoming_amt_msat,
+           outgoing_amt_msat: outgoing_amt_msat,
+           incoming_timelock: incoming_timelock,
+           outgoing_timelock: outgoing_timelock
+         }
+       }) do
     %{
       amount_in: incoming_amt_msat,
       amount_out: outgoing_amt_msat,
@@ -151,17 +161,17 @@ defmodule Steer.Lnd.Subscriptions.Htlc do
     }
   end
 
-  defp extract_link_fail_event_map %Routerrpc.LinkFailEvent{
-    failure_detail: failure_detail,
-    failure_string: failure_string,
-    wire_failure: wire_failure,
-    info: %Routerrpc.HtlcInfo{
-      incoming_amt_msat: incoming_amt_msat,
-      outgoing_amt_msat: outgoing_amt_msat,
-      incoming_timelock: incoming_timelock,
-      outgoing_timelock: outgoing_timelock
-      }
-  } do
+  defp extract_link_fail_event_map(%Routerrpc.LinkFailEvent{
+         failure_detail: failure_detail,
+         failure_string: failure_string,
+         wire_failure: wire_failure,
+         info: %Routerrpc.HtlcInfo{
+           incoming_amt_msat: incoming_amt_msat,
+           outgoing_amt_msat: outgoing_amt_msat,
+           incoming_timelock: incoming_timelock,
+           outgoing_timelock: outgoing_timelock
+         }
+       }) do
     %{
       amount_in: incoming_amt_msat,
       amount_out: outgoing_amt_msat,
@@ -173,7 +183,7 @@ defmodule Steer.Lnd.Subscriptions.Htlc do
     }
   end
 
-  defp broadcast htlc_event, topic, message do
+  defp broadcast(htlc_event, topic, message) do
     Endpoint.broadcast(topic, message, htlc_event)
 
     htlc_event
