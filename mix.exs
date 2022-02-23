5c16541bc9542cb0068b4f3bc06e0be3096435cb
RooSoft <marc@roosoft.com>
2022-02-23 14:42:45 -0500

    version 0.2.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Steer.MixProject do
   def project do
     [
       app: :steer,
-      version: "0.2.3.2",
+      version: "0.2.4gi",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
@@ -32,7 +32,7 @@ defmodule Steer.MixProject do
   defp releases() do
     [
       steer: [
-        version: "0.2.3.2",
+        version: "0.2.4",
         applications: [
           steer: :permanent
         ]
