c4151e46506e73a826cd935bc09b19e9ba7e6570
RooSoft <marc@roosoft.com>
2022-04-04 08:10:09 -0400

    using CTEs to get the channel list

--- lib/steer/repo/queries/get_all_channels.ex
+++ lib/steer/repo/queries/get_all_channels.ex
@@ -19,41 +19,28 @@ defmodule Steer.Repo.Queries.GetAllChannels do
   end
 
   defp get_all_channels(%{include_closed: false}) do
-    from(c in Models.Channel,
-      join: fi in subquery(forwards_in_subquery()),
-      on: c.id == fi.channel_id,
-      join: fo in subquery(forwards_out_subquery()),
-      on: c.id == fo.channel_id,
-      where: c.status != :closed,
-      order_by: [
-        desc:
-          fragment(
-            "SELECT coalesce(Max(?, ?), 0)",
-            fi.latest_timestamp,
-            fo.latest_timestamp
-          )
-      ],
-      select: %{
+    from(
+      Models.Channel
+      |> with_cte("forwards", as: ^forwards_cte())
+      |> join(:left, [c], f in "forwards", on: c.id == f.channel_id)
+      |> where([c], c.status != :closed)
+      |> order_by([c, f], [{:desc, f.latest}])
+      |> select([c, f], %{
         id: c.id,
         lnd_id: c.lnd_id,
         alias: c.alias,
         color: c.color,
+        status: c.status,
         capacity: c.capacity,
         local_balance: c.local_balance,
         remote_balance: c.remote_balance,
         node_pub_key: c.node_pub_key,
-        forward_in_count: fi.forward_count,
-        forward_out_count: fo.forward_count,
-        latest_forward_in_time: fi.latest_timestamp,
-        latest_forward_out_time: fo.latest_timestamp,
-        latest_forward_time:
-          fragment(
-            "SELECT Max(?, ?)",
-            fi.latest_timestamp,
-            fo.latest_timestamp
-          ),
-        status: c.status
-      }
+        forward_in_count: f.nb_input,
+        forward_out_count: f.nb_output,
+        latest_forward_in_time: f.latest_input,
+        latest_forward_out_time: f.latest_input,
+        latest_forward_time: f.latest
+      })
     )
   end
 
@@ -79,5 +66,26 @@ defmodule Steer.Repo.Queries.GetAllChannels do
         latest_timestamp: max(f.timestamp_ns)
       }
     )
+  end
+
+  defp forwards_cte() do
+    Models.Channel
+    |> with_cte("forwards_in", as: ^forwards_in_subquery())
+    |> with_cte("forwards_out", as: ^forwards_out_subquery())
+    |> join(:left, [c], fi in "forwards_in", on: c.id == fi.channel_id)
+    |> join(:left, [c], fo in "forwards_out", on: c.id == fo.channel_id)
+    |> select([c, fi, fo], %{
+      channel_id: c.id,
+      nb_input: fi.forward_count,
+      nb_output: fo.forward_count,
+      latest_input: fi.latest_timestamp |> coalesce(0),
+      latest_output: fo.latest_timestamp |> coalesce(0),
+      latest:
+        fragment(
+          "SELECT Max(?, ?)",
+          fi.latest_timestamp |> coalesce(0),
+          fo.latest_timestamp |> coalesce(0)
+        )
+    })
   end
 end
