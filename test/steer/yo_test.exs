f888a171fdabc9ba40bcae3903ebcc0cf6279bad
RooSoft <marc@roosoft.com>
2022-04-26 08:57:19 -0400

    testing Steer.Formatting.Channel

---
+++ test/steer/yo_test.exs
@@ -0,0 +1,21 @@
+defmodule SteerTest.Formatting.Channel do
+  use ExUnit.Case
+
+  describe "compressed_pub_key/1" do
+    setup do
+      %{pub_key: "037b6d303c95b4faf2f62a214cc32c78aa0ded8ab5bd7a11aaa4883bbe292a4764"}
+    end
+
+    test "success: returns a string of length 14", %{pub_key: pub_key} do
+      short_version = Steer.Formatting.Channel.compressed_pub_key(pub_key)
+
+      assert String.length(short_version) == 14
+    end
+
+    test "success: returns a string containing double dots in the middle", %{pub_key: pub_key} do
+      short_version = Steer.Formatting.Channel.compressed_pub_key(pub_key)
+
+      assert String.slice(short_version, 6, 2) == ".."
+    end
+  end
+end
