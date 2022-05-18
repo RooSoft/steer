3550434268639a1286f98ea0df4b9f3f5a3517e6
RooSoft <marc@roosoft.com>
2022-05-18 12:44:09 -0400

    removed minor warnings

--- .iex.exs
+++ .iex.exs
@@ -4,9 +4,10 @@ alias Steer.Repo, as: Repo
 alias Steer.Lightning.Models, as: Models
 
 timestamp = fn ->
-  {_date, {hour, minute, _second}} = :calendar.local_time
+  {_date, {hour, minute, _second}} = :calendar.local_time()
+
   [hour, minute]
-  |> Enum.map(&(String.pad_leading(Integer.to_string(&1), 2, "0")))
+  |> Enum.map(&String.pad_leading(Integer.to_string(&1), 2, "0"))
   |> Enum.join(":")
 end
 
@@ -17,22 +18,22 @@ IEx.configure(
       atom: :light_cyan,
       string: :light_black,
       boolean: :red,
-      nil: [:magenta, :bright],
+      nil: [:magenta, :bright]
     ],
     ls_directory: :cyan,
     ls_device: :yellow,
     doc_code: :green,
     doc_inline_code: :magenta,
     doc_headings: [:cyan, :underline],
-    doc_title: [:cyan, :bright, :underline],
-  ],
+    doc_title: [:cyan, :bright, :underline]
+  ]
 )
 
 IEx.configure(
   default_prompt:
-    "#{IO.ANSI.green}%prefix#{IO.ANSI.reset} " <>
-    "[#{IO.ANSI.magenta}#{timestamp.()}#{IO.ANSI.reset} " <>
-    ":: #{IO.ANSI.cyan}%counter#{IO.ANSI.reset}] >"
+    "#{IO.ANSI.green()}%prefix#{IO.ANSI.reset()} " <>
+      "[#{IO.ANSI.magenta()}#{timestamp.()}#{IO.ANSI.reset()} " <>
+      ":: #{IO.ANSI.cyan()}%counter#{IO.ANSI.reset()}] >"
 )
 
-channels = Repo.get_all_channels
+channels = Repo.get_all_channels()
--- lib/steer_web/live/diagnostics_live/graph.ex
+++ lib/steer_web/live/diagnostics_live/graph.ex
@@ -1,23 +1,23 @@
-defmodule SteerWeb.DiagnosticsLive.Graph do
-  use Phoenix.Component
+# defmodule SteerWeb.DiagnosticsLive.Graph do
+#   use Phoenix.Component
 
-  alias Steer.GraphRepo
+#   alias Steer.GraphRepo
 
-  def graph(assigns) do
-    node_count = GraphRepo.get_number_of_nodes()
+#   def graph(assigns) do
+#     node_count = GraphRepo.get_number_of_nodes()
 
-    ~H"""
-    <div class="diagnostics-graph-info">
-      <%= node_count %> nodes
-    </div>
+#     ~H"""
+#     <div class="diagnostics-graph-info">
+#       <%= node_count %> nodes
+#     </div>
 
-    <div class="diagnostics-graph-status">
-      Graph status: <em><%= assigns.status %></em>
-    </div>
+#     <div class="diagnostics-graph-status">
+#       Graph status: <em><%= assigns.status %></em>
+#     </div>
 
-    <button phx-click="refresh_graph">
-      refresh
-    </button>
-    """
-  end
-end
+#     <button phx-click="refresh_graph">
+#       refresh
+#     </button>
+#     """
+#   end
+# end
