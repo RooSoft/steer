cf0c336239f08c577041bd4c586e0dc6e6b15228
RooSoft <marc@roosoft.com>
2022-05-19 12:35:18 -0400

    added different element checks in home_test

--- test/steer_web/live/home_test.exs
+++ test/steer_web/live/home_test.exs
@@ -4,7 +4,12 @@ defmodule SteerWeb.Live.HomeTest do
   import Phoenix.LiveViewTest
 
   test "GET /", %{conn: conn} do
-    {:ok, _view, html} = live(conn, "/")
+    {:ok, view, html} = live(conn, "/")
+
     assert html =~ "Steer"
+    assert view |> element("span.home-live-channel-number", "0") |> has_element?
+    assert view |> element("div[data-phx-component=1]") |> has_element?
+    assert view |> element("p.alert-info") |> has_element?
+    assert view |> element("div.home-live-channel-count") |> has_element?
   end
 end
