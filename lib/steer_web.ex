6b99179d3e33d307d0eed799077b4a97128e4f11
roosoft <marc@roosoft.com>
2022-10-07 10:18:46 -0400

    completed migration to Phoenix LiveView 0.18, reinstating the live flashes

--- lib/steer_web.ex
+++ lib/steer_web.ex
@@ -83,6 +83,7 @@ defmodule SteerWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
+      import Phoenix.Component
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
