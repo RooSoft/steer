f4f44083158dc90ee116b2135ecb51622b7e9fa8
roosoft <marc@roosoft.com>
2022-10-11 09:37:03 -0400

    refactored SteerWeb.DiagnosticsLive.Liquidity to meet new phoenix liveview standards

--- lib/steer_web/live/diagnostics_live/liquidity.ex
+++ lib/steer_web/live/diagnostics_live/liquidity.ex
@@ -2,25 +2,56 @@ defmodule SteerWeb.DiagnosticsLive.Liquidity do
   use Phoenix.Component
 
   def liquidity(assigns) do
-    local_balance =
-      assigns.channels
-      |> Enum.reduce(0, fn channel, funds ->
-        funds + channel.local_balance
-      end)
-
-    remote_balance =
-      assigns.channels
-      |> Enum.reduce(0, fn channel, funds ->
-        funds + channel.remote_balance
-      end)
+    assigns =
+      assigns
+      |> assign_formatted_local_balance()
+      |> assign_formatted_remote_balance()
 
     ~H"""
     <div>
-      Local balance: <%= Number.SI.number_to_si(local_balance/1000, unit: "", precision: 1) %> sats
+      Local balance: <%= @local_balance %> sats
     </div>
     <div>
-      Remote balance: <%= Number.SI.number_to_si(remote_balance/1000, unit: "", precision: 1) %> sats
+      Remote balance: <%= @remote_balance %> sats
     </div>
     """
+  end
+
+  defp assign_formatted_local_balance(assigns) do
+    formatted =
+      assigns
+      |> get_local_balance()
+      |> format_balance
+
+    assigns
+    |> assign(:local_balance, formatted)
+  end
+
+  defp assign_formatted_remote_balance(assigns) do
+    formatted =
+      assigns
+      |> get_remote_balance()
+      |> format_balance
+
+    assigns
+    |> assign(:remote_balance, formatted)
+  end
+
+  defp get_local_balance(assigns) do
+    assigns.channels
+    |> Enum.reduce(0, fn channel, funds ->
+      funds + channel.local_balance
+    end)
+  end
+
+  defp get_remote_balance(assigns) do
+    assigns.channels
+    |> Enum.reduce(0, fn channel, funds ->
+      funds + channel.remote_balance
+    end)
+  end
+
+  defp format_balance(balance) do
+    Number.SI.number_to_si(balance / 1000, unit: "", precision: 1)
   end
 end
